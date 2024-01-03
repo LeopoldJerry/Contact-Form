60579233e15605a1b60e2e8ad28d2e2af86343da
Egor <145351359+LeopoldJerry@users.noreply.github.com>
2024-01-03 14:32:05 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package itproger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ContactForm form = new ContactForm();
+        form.setVisible(true);
+
+    }
+}
+
